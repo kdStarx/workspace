b77dd562cb1bd8bcc03305f6203013a05d606778
Kavis Pandey <kavis.pandey@hpe.com>
2016-08-30 00:35:41 +0530

    Adding working code

--- FileReader/src/main/java/FileReader.java
+++ FileReader/src/main/java/FileReader.java
@@ -1,6 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by pandeyk on 8/29/2016.
  */
 public class FileReader {
+    private List<String> linesList = new ArrayList<String>();
+
+    public List<String> getLinesList() {
+        return linesList;
+    }
+
+    public void setLinesList(List<String> linesList) {
+        this.linesList = linesList;
+    }
+
+    // Add lines to existing list
+    private List<String> linesList(String line) {
+        System.out.println("Size of list before reading line "+linesList.size());
+        System.out.println("Line to be added "+line);
+        linesList.add(line);
+        System.out.println("Size of list after reading line "+linesList.size());
+        return linesList;
+    }
+
+    public InputStream fileToStream (String fileName) {
+        System.out.println("FIleName is "+fileName);
+        InputStream stream = getClass().getResourceAsStream(fileName);
+        return stream;
+    }
+
+    public List<String> streamToList(InputStream inputStream, List<String> list) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = bufferedReader.readLine())!=null){
+            list = linesList(line);
+        }
+        return list;
+    }
+
+    public List<String> sortedList (List<String> list) {
+        Collections.sort(list);
+        return list;
+    }
 
 }
---
+++ FileReader/src/main/java/FileReaderMain.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by pandeyk on 8/29/2016.
+ */
+public class FileReaderMain {
+
+    public static void main(String[] args) throws IOException {
+        String fileName = "Sample";
+        FileReader fileReader = new FileReader();
+
+        List<String> oldList = fileReader.streamToList(fileReader.fileToStream(fileName), new ArrayList<String>());
+        System.out.println("Sorted List ..");
+        List<String> sortedList = fileReader.sortedList(oldList);
+
+        Iterator iterator = sortedList.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+    }
+}
